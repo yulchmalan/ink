5f230c31c1546c3de61dbec0c97279cec1f31d62
yulch <juliamalanjuk@gmail.com>
2025-04-22 20:50:31 +0300

    +key for api

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
-import indexRouter from "./routes/index.js";
-import authRouter from "./routes/auth.js";
-import { connectDB } from "./config/db.js";
-import { typeDefs, resolvers } from "./graphql/index.js";
 import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs, resolvers } from "./graphql/index.js";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
+
+const app = express();
 const port = 3000;
 
-//server setup
+connectDB();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -20,16 +19,24 @@ const server = new ApolloServer({
 
 await server.start();
 
-//stara pizda
-const app = express();
-dotenv.config();
 app.use(express.json());
 
-app.get("/", indexRouter);
-app.get("/auth", authRouter);
-app.use("/graphql", expressMiddleware(server));
+app.use(
+  "/graphql",
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+      const apiKey = req.headers["x-api-key"];
+      console.log("API KEY RECEIVED:", apiKey);
+
+      if (!apiKey || apiKey !== process.env.API_KEY) {
+        throw new Error("Invalid or missing API key");
+      }
+
+      return {};
+    },
+  })
+);
 
 app.listen(port, () => {
-  connectDB();
-  console.log(`server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/graphql`);
 });
