9846986e73ec288d7c48cb6de197c56f4a8cec7f
yulch <juliamalanjuk@gmail.com>
2025-04-26 19:25:12 +0300

    ленг світч готовий (за функціоналом, візуал чєкавостка люта)

--- client/app/[locale]/layout.tsx
+++ client/app/[locale]/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { cookies } from "next/headers";
-import "../globals.css";
+import "../../styles/globals.scss";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- client/app/[locale]/page.tsx
+++ client/app/[locale]/page.tsx
@@ -1,8 +1,14 @@
+import LanguageSwitcher from "@/components/UI/LanguageSwitcher";
 import { getTranslations, getLocale } from "next-intl/server";
 
 export default async function Home() {
   const locale = await getLocale();
   const t = await getTranslations({ locale, namespace: "Index" });
 
-  return <h1>{t("localeText")}</h1>;
+  return (
+    <>
+      <h1>{t("localeText")}</h1>
+      <LanguageSwitcher></LanguageSwitcher>
+    </>
+  );
 }
