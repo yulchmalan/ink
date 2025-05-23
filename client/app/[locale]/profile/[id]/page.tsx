01e172e814e17371bcc95ae1740fb46afe44402c
yulch <juliamalanjuk@gmail.com>
2025-05-23 04:44:17 +0300

    ZNOWU PROPSY ANNIMNO TYPIZOWANI

--- client/app/[locale]/profile/[id]/page.tsx
+++ client/app/[locale]/profile/[id]/page.tsx
@@ -4,11 +4,7 @@ import Container from "@/components/Layout/Container/Container";
 import { getLocale, getTranslations } from "next-intl/server";
 import { GET_USER } from "@/graphql/queries/getUser";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { locale: string };
-}) {
+export async function generateMetadata({ params }: any) {
   const { locale } = params;
 
   const t = await getTranslations({ locale: locale, namespace: "Meta" });
